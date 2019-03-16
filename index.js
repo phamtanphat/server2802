46451805f74182dedcdd45d6c13452b4cdc022ba
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-03-16 14:56:47 +0700

    post

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const {json} = require('body-parser');
 
 const app = express();
+
+app.use(json());
 
 app.get('/cong/:a/:b' , (req, res) => {
     const {a , b} = req.params;
@@ -11,6 +14,17 @@ app.get('/cong/:a/:b' , (req, res) => {
         return res.send({success : false , message : "DIVIDE_BY_ZERO"});
     }
     const result = +a + +b;
+    res.send({success : true , result});
+})
+app.post('/chia' , (req, res) => {
+    const {a , b} = req.body;
+    if(isNaN(a) || isNaN(b)){
+        return res.send({success : false , message : "INVALID_TYPE"});
+    }
+    if(b == 0){
+        return res.send({success : false , message : "DIVIDE_BY_ZERO"});
+    }
+    const result = +a / +b;
     res.send({success : true , result});
 })
 
